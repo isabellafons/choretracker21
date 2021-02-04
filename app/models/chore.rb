4cd2fe9a2e30100b8768da50667eb08abc02d0f6
isabellafons <isabellafons@gmail.com>
2021-02-04 06:48:10 -0500

    finished chore model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,21 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
+
+    validates_date :due_on
+
+    scope :by_task, -> {joins(:task).order('name')}
+    scope :chronological, -> {order(:due_on,:completed)}
+    scope :pending, -> {where(completed: false)}
+    scope :done, -> {where(completed:true)}
+    scope :upcoming, -> {where('due_on>=?', Date.today)}
+    scope :past, -> {where('due_on<?',Date.today)}
+
+    def status
+        if self.completed == true
+            return "Completed"
+        else
+            return "Pending"
+        end
+    end
 end
