3a59c240382001e435bcda6d2a4cdf8ee331e378
isabellafons <isabellafons@gmail.com>
2021-02-03 22:14:42 -0500

    c tests

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,5 +4,12 @@ class Child < ApplicationRecord
 
     validates_presence_of :first_name
     validates_presence_of :last_name
+
+    scope :alphabetical, -> { order('last_name,first_name') }
+    scope :active, -> { where(active:true) }
+
+    def name
+        first_name + " " + last_name
+    end
     
 end
